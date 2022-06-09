03d457c93d96c180eb73aec6e9285f15db54c7e2
laserriot <106926476+laserriot@users.noreply.github.com>
2022-06-09 10:44:29 +0200

    Update vars.ts

--- src/util/vars.ts
+++ src/util/vars.ts
@@ -2,7 +2,7 @@ import {getenv} from "./utils";
 
 /* SETTINGS */
 export const globalTargetBalance = +(getenv('ICETRACKER_TARGET_BALANCE', '250'));
-export const csvUrl = getenv('ICETRACKER_CSV_URL', '')
+export const tsvUrl = getenv('ICETRACKER_TSV_URL', '')
 export const refillLevel = +(getenv('ICETRACKER_REFILL_LEVEL', '7')) / 100.0;
 export const takeProfitLevel = +(getenv('ICETRACKER_TAKEPROFIT_LEVEL', '7')) / 100.0;
 
